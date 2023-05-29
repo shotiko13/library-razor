cb425bd6cf8311ef71109b0e79c58938ab46cd92
k3rnel_panic <shotov1303@gmail.com>
2023-05-29 19:54:32 +0400

    things done

---
+++ Model/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
