71956c2dff1a73f2d093f9df013a3fd6a2606ffa
k3rnel_panic <shotov1303@gmail.com>
2023-05-30 14:37:15 +0400

    added book get API

---
+++ Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryRazor.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
